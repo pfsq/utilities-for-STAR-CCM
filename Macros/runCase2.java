195ec004fbd07b088ef04b4ffece838362863fda
Pablo Fernandez <pablofernandez.sq@gmail.com>
2014-02-11 18:16:35 +0100

    New file runCase2.java

---
+++ Macros/runCase2.java
@@ -0,0 +1,27 @@
+/**
+ * Update of runCase.java using MacroUtils v3.
+ * 
+ * @author Pablo Fernandez (pfernandez@theansweris27.com)
+ */
+
+import macroutils.*;
+
+public class runCase2 extends MacroUtils {
+    
+    private int xres = 1280;
+    private int yres = 720;
+
+    public void execute() {
+        _initUtils();
+        clearSolution();
+        initializeSolution();
+        runCase(true);
+        saveSimWithSuffix("@finished");
+        postProcessing();
+    }
+    
+    // Create elements for post-processing.
+    void postProcessing() {
+        hardCopyPictures(xres, yres, "\\imagenes\\" + simTitle);
+    }
+}
